7f577187adc948de3f3fc5c8026380d5b53b959c
fahminurdiansyah <alexxela3031@gmail.com>
2026-04-30 15:33:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const container = entry.target.querySelector('.container');
+
+    if (entry.isIntersecting) {
+      container.classList.add('show');
+    } else {
+      container.classList.remove('show');
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+document.querySelectorAll('section').forEach(section => {
+  observer.observe(section);
+});
